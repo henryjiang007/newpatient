77e4a06fdc56e0c32626bf39ef36069cc767aca8
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2022-07-19 14:03:17 -0700

    new

--- service/resources/pdfgenerator.py
+++ service/resources/pdfgenerator.py
@@ -12,56 +12,109 @@ from .hooks import validate_access
 class PDFGenerator():
     """PDFGenerator class"""
     # pylint: disable=no-self-use
+    api_key = ''
+
     def on_post(self, req, resp):
         """ Implement POST """
         # pylint: disable=broad-except,no-member
-        output_pdf = ''
         try:
-            data = json.loads(req.bounded_stream.read())
+            data =  self.retrieve_submissions()
             if data:
                 basename = os.path.dirname(__file__)
-                output_file = utils.write_fillable_pdf(basename, data['request']['data'])
-                #print(output_file)
-                self.send_email(data['request'], output_file)
+                for request in data:
+                    output_file = utils.write_fillable_pdf(basename, request['data'])
+                    result = self.send_email(request['data'], output_file)
+                    submission_id = request['_id']
+                    if result == 200:
+                        self.delete_submission(PDFGenerator.api_key, submission_id)
         except Exception as error:
             print(f"Failed to generate PDF: {error}")
             print(traceback.format_exc())
             resp.status = falcon.HTTP_500   # pylint: disable=no-member
             resp.text = json.dumps(str(error))
-        #finally: # clean up
-            #if len(output_pdf) > 1:
-                #os.remove(output_file)
+
+    def retrieve_submissions(self):
+        """ retrieve submissions"""
+        email = os.environ.get('EMAIL')
+        password = os.environ.get('PASSWORD')
+        login_url = 'https://formio.form.io/user/login/submission?live=1'
+
+        payload = json.dumps({
+                "data": {
+                   "email":email,
+                    "password": password
+                }
+            })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        try:
+            response = requests.request("POST", login_url, headers=headers, data=payload)
+            jwt_token = response.headers['x-jwt-token']
+            url = "https://jotielkyhqtrqtb.form.io/newpatientregistration/submission?limit=10"
+            payload={}
+            headers = {
+                'x-jwt-token': jwt_token
+            }
+            PDFGenerator.api_key = jwt_token
+            response = requests.request("GET", url, headers=headers, data=payload)
+            return response.json()
+        except Exception as err: # pylint: disable=broad-except
+            print("login error: {0}".format(err))
+            return None
+
+    def delete_submission(self, api_key, submission_id):
+        """helper function for deleting a submission on formio"""
+
+        delete_url = 'https://jotielkyhqtrqtb.form.io/newpatientregistration/submission/' + str(submission_id)
+        payload = {}
+        files = {}
+        headers = {
+            'x-jwt-token': api_key
+        }
+        print(submission_id)
+        try:
+            response = requests.request("DELETE", delete_url, headers=headers, \
+                data=payload, files=files)
+            response.raise_for_status()
+            return response
+        except Exception as err: # pylint: disable=broad-except
+            print("deletion error: {0}".format(err))
+            return None
 
     # pylint: disable=no-self-use, too-many-locals
     def send_email(self, request, output_pdf):
         """
         send emails
         """
-        subject = "New patient registration for " + request['data']['patientName']
+        subject = "New patient registration for " + request['patientName']
         email_to = []
-        for email in request['emails']['to']:
-            email_to.append({
-                "email": email["email"],
-                "name": email["name"]
-            })
+        email_to.append({
+            "email": 'ansondentalstudio@gmail.com',
+            "name": 'Anson Dental'
+        })
 
+        html_content  = "<html><body><table>"
+        for key, value in request.items():
+            html_content = html_content + "<tr><td>" + key + "</td><td>" + str(value) + "</td></tr>"
+        html_content = html_content + "</table></body></html>"
         file_name = "new_patient.pdf"
         payload = {
             "subject": subject,
             "attachments": [
                 {
                     "content": "",
-                    "path": os.environ.get('BASE_URL') + "static/" + output_pdf,
+                    "path": os.environ.get('BASE_URL') + "/static/" + output_pdf,
                     "filename": file_name,
                     "type": "application/pdf"
                 }
             ],
             "to": email_to,
-            "from": request['emails']['from'],
+            "from": {"email": "jiang.henry@gmail.com", "name":  "Anson Dental"},
             "content": [
                 {
                     "type": "text/html",
-                    "value": "<html><p>New Patient submission</p></html>"
+                    "value": html_content
                 },
                 {
                     "type": "text/custom",
@@ -90,4 +143,4 @@ class PDFGenerator():
         except requests.exceptions.RequestException as err:
             logging.exception("OOps: Something Else: %s", err)
 
-        return result
+        return result.status_code
